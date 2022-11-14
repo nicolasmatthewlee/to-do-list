f24db01ce4dd938f32c36561af370fb74f7dd3c1
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2022-11-13 20:03:32 -0800

    setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname,'dist'),
+        clean: true,
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: 'To-Do List'
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader','css-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource'
+            }]
+    }
+}
